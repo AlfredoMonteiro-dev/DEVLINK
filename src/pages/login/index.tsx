ce237c465e70c8bb96c83802bc2183b30b8fa6b0
AlfredoMonteiro-dev <alfredolima017@gmail.com>
2026-07-08 14:33:58 -0400

    form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,47 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+import { useState } from "react";
+import type { SubmitEvent } from "react";
 
 export function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: SubmitEvent){
+          e.preventDefault();
+
+          console.log({
+            email: email,
+            password: password
+          })
+    }
+
     return(
         <div className="flex w-full h-screen justify-center items-center flex-col">
             <Link to="/">
                 <h1 className="mt-11 text-white mb-7 font-bold text-5xl">DEV
                 <span className="bg-linear-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span></h1>
             </Link>
+
+            <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2">   
+                <Input
+                    placeholder="Digite seu e-mail..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <Input
+                    placeholder="********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                <button
+                type="submit"
+                className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white ">
+                    Acessar
+                </button>
+            </form>
         </div>
     )
 }
